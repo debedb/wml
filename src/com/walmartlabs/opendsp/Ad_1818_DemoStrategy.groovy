c5b91073f55f3f5fd3dc7fde4b223e5fa59d258a
ui-odsp <ui@opendsp.com>
2016-05-31 18:14:07 +0300

    UI auto - create Ad_1818----2016-05-31T15:14:07UGMTC-8f4c2

---
+++ src/com/walmartlabs/opendsp/Ad_1818_DemoStrategy.groovy
@@ -0,0 +1,45 @@
+package com.enremmeta.rtb.groovy.tc
+
+import com.enremmeta.rtb.api.AdImpl
+
+import com.enremmeta.rtb.api.TargetingGeo
+
+
+
+
+
+
+
+
+
+class Ad_1818_DemoStrategy extends AdImpl {
+
+	void init() {
+
+		adomain = ["test.com"]
+		desc = "Demo Strategy"
+		bidAmount = 0
+		iurl = "http://creative.us.s3.opendsp.com/creatives/2184/myip300x250_updated.jpg"
+                
+		tags = [
+			new Tag_2184_DemoCreative_1818_DemoStrategy(this)
+		]
+                
+                adVersion=6
+                
+                
+ 		clientId="136"
+		campaignId="313"
+		advertiser="Demo Advertiser"
+		advertiserId="236"
+
+		abTesting = true
+		abTestingControlShare = 1
+
+ 		geos = [
+			new TargetingGeo(null,null,null,"us",null)
+		]
+
+                
+	}
+}
