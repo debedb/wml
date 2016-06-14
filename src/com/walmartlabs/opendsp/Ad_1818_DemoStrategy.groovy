0182bb2d17365b034e1eeaac14d85b80456fbe9e
ui-odsp <ui@opendsp.com>
2016-06-14 04:03:00 +0300

    UI auto - create Ad_1818----2016-06-14T01:02:59UGMTC-7b517

--- src/com/walmartlabs/opendsp/Ad_1818_DemoStrategy.groovy
+++ src/com/walmartlabs/opendsp/Ad_1818_DemoStrategy.groovy
@@ -21,13 +21,13 @@ class Ad_1818_DemoStrategy extends AdImpl {
 		iurl = "http://creative.us.s3.opendsp.com/creatives/2184/myip300x250_updated.jpg"
                 
                 bidAmount = 0
-                bidPriceCalculator = new BidPriceCalculator_1();
+                
                 
 		tags = [
 			new Tag_2184_DemoCreative_1818_DemoStrategy(this)
 		]
                 
-                adVersion=12
+                adVersion=13
                 
                 
  		clientId="136"
@@ -41,6 +41,8 @@ class Ad_1818_DemoStrategy extends AdImpl {
  		geos = [
 			new TargetingGeo(null,null,null,"us",null)
 		]
+
+ 		targetingSegments="OR(\"386:fp:236\",\"387:fp:236\")"
 
                 
 	}
